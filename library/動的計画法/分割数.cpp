41847c24dd54f9378cd75bc2cdb1ac55a9c9e4e6
Kuri174 <oxyshower@gmail.com>
2020-10-03 12:01:25 +0900

    mod

--- library/動的計画法/分割数.cpp
+++ library/動的計画法/分割数.cpp
@@ -1,44 +1,46 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define int long long
 
 #ifdef LOCAL_DEBUG
-  #include "LOCAL_DEBUG.hpp"
+#include "LOCAL_DEBUG.hpp"
 #endif
 
-const int MOD = 1e9+7;
+const int mod = 1e9 + 7;
 
-//http://incognita.hatenablog.com/entry/20110305/1299344781
-int PartNum(int n,int m,int k){ //nがm以下の自然数の和 k個で構成される分割数
-  vector<vector<int>> dp(n+1,vector<int> (m+1,0)); //iをj以下の自然数の和に分ける場合の数
+// http://incognita.hatenablog.com/entry/20110305/1299344781
+
+/* ----- nがm以下の自然数の和k個で構成される分割数 ----- */
+int PartNum(int n, int m, int k) {
+  /* ----- iをj以下の自然数の和に分ける場合の数 ----- */
+  vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
   dp[0][0] = 1;
-  for(int i = 0; i <= n; i++){
-    for(int j = 1; j <= m; j++){
-      if(i-j >= 0){
-        dp[i][j] = dp[i-j][j] + dp[i][j-1];
-      }
-      else {
+  for (int i = 0; i <= n; i++) {
+    for (int j = 1; j <= m; j++) {
+      if (i - j >= 0) {
+        dp[i][j] = dp[i - j][j] + dp[i][j - 1];
+      } else {
         dp[i][j] = dp[i][i];
       }
-      if(i-j-k >= 0){
-        dp[i][j] += MOD - dp[i-j-k][j-1];
+      if (i - j - k >= 0) {
+        dp[i][j] += mod - dp[i - j - k][j - 1];
       }
-      dp[i][j] %= MOD;
+      dp[i][j] %= mod;
     }
   }
   return dp[n][m];
 }
 
-//https://yukicoder.me/problems/no/269
-signed main(){
-
-  int n,s,k; cin >> n >> s >> k;
-  s -= (n-1)*k*n/2;
-  if(s < 0){
+// https://yukicoder.me/problems/no/269
+signed main() {
+  int n, s, k;
+  cin >> n >> s >> k;
+  s -= (n - 1) * k * n / 2;
+  if (s < 0) {
     cout << 0 << endl;
     return 0;
   }
-  cout << PartNum(s,n,1<<30) << endl;
+  cout << PartNum(s, n, 1 << 30) << endl;
 
   // //https://atcoder.jp/contests/joi2009yo/tasks/joi2009yo_f
   // int n,m,s; cin >> n >> m >> s;
